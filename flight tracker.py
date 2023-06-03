0eb8a50061ead6444ad703b9bd7ac0924f8ad0e2
jxlee007 <107770093+jxlee007@users.noreply.github.com>
2023-06-03 18:21:27 +0530

    flight tracker.py
    The given code is part of a flight tracker application. The team working on this project has added explanations to the code but the computer returns an error.
    
    Task
    Use comments to add the explanations in a way that doesn't result in errors

---
+++ flight tracker.py
@@ -0,0 +1,10 @@
+# Storing the flight number
+flight_n = "BA0117"
+
+# Storing the flight information
+destination = "New York"
+distance = 1580
+
+print(flight_n)
+print(destination)
+print(distance)
