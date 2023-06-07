9f13828e1a497399a3fed2fd09d858e3a138b600
jxlee007 <107770093+jxlee007@users.noreply.github.com>
2023-06-07 18:39:11 +0530

    Create smart parking lot.py
    A smart parking lot displays different messages to the visitor based on the number of available spaces.
    Complete the program to inform the user about available spaces in the parking lot

---
+++ smart parking lot.py
@@ -0,0 +1,10 @@
+# Take the number of available spaces as an input
+spaces = int(input())
+
+# Display message if spaces are available
+if spaces > 0:
+    print("Available spaces")
+
+# Display a different message if spaces are not available
+else:
+    print("Sorry, the parking lot is full")
