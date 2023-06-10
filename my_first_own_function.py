e1fb40272f3a4e7a9ab69112d44fe26025ba7adf
jxlee007 <107770093+jxlee007@users.noreply.github.com>
2023-06-10 20:35:57 +0530

    Update my_first_own_function.py

--- my_first_own_function.py
+++ my_first_own_function.py
@@ -3,10 +3,11 @@ def hello():
 hello()
 # while doing i am happy
 # to call function for output no print is needed
-
+#-----------------------------------------------------------------------
 # here first i declared the function
 # then inside paranthesis of function taken a variable
 def output(msg):
   print(msg + " fcuk")
 output("spam")
 #spam fcuk
+# arguments can be used as variables inside the function.
