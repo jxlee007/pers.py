f40148990bfd3aa48a906837aaf1abd62387a105
jxlee007 <107770093+jxlee007@users.noreply.github.com>
2023-06-15 10:05:17 +0530

    NEF.py
    You are working on data that represents the economic freedom rank by country.
    
    Each country name and rank are stored in a dictionary, with the key being the country name.
    
    Complete the program to take the country name as input and output its corresponding economic freedom rank.
    
    In case the provided country name is not present in the data, output "Not found"

---
+++ NEF.py
@@ -0,0 +1,15 @@
+data = {
+    'Singapore': 1,
+    'Ireland': 6,
+    'United Kingdom': 7,
+    'Germany': 27,
+    'Armenia': 34,
+    'United States': 17,
+    'Canada': 9,
+    'Italy': 74
+}
+
+choice = input()
+print (data.get(choice,"Not found"))
+#using get function
+#using dictionary
