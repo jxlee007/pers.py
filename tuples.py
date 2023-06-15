456333696e863a70575dd00ebcf953149fc9a1f6
jxlee007 <107770093+jxlee007@users.noreply.github.com>
2023-06-15 11:10:55 +0530

    Create tuples.py
    You are given a list of contacts, where each contact is represented by a tuple, with the name and age of the contact.
    
    Complete the program to get a string as input, search for the name in the list of contacts and output the age of the contact

---
+++ tuples.py
@@ -0,0 +1,19 @@
+contacts = [
+    ('James', 42),
+    ('Amy', 24),
+    ('John', 31),
+    ('Amanda', 63),
+    ('Bob', 18)
+]
+
+name = input()
+
+for x in contacts:
+    if name in x:
+        print (str(x[0]) +" is "+ str(x[1]))
+        break
+else:
+    print("Not Found")
+# using tuples in list
+# not my logic
+# need great help
