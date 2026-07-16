4875cff2c61495de611038ee631ebe2688123b06
jxlee007 <jimmyfalco007@gmail.com>
2026-07-16 12:13:13 +0530

    created an oops checklist - to build understaning by practicing OOPs in correct way not ai

--- models TBD/W3S/oops-email.py
+++ models TBD/W3S/oops-email.py
@@ -1,6 +1,7 @@
 #  we have 3 classes in this program
 #  Email-5, User-1, Inbox - as complex DS
 
+# Email
 class Email:
 #  In email class we have 5 params
     def __init__(
@@ -11,6 +12,7 @@ class Email:
             body:str
         )-> None :
         
+# Holds information like sender, receiver, subject, body
         # PIA - unique public instance attr 
         self.sender = sender
         self.receiver = receiver
@@ -18,10 +20,11 @@ class Email:
         self.body = body
         self.read = False
 
-    # 
+    
     def mark_as_read(self):
         self.read = True
 
+# Represents one email message
     def display_full_email(self):
         self.mark_as_read()
         print('\n--- Email ---')
@@ -31,36 +34,23 @@ class Email:
         print(f'Body: {self.body}')
         print('------------\n')
 
-    # special dunder method
+    # special dunder method - for convert email into readable str 
     def __str__(self):
         status = 'Read' if self.read else 'Unread'
         return f"[{status}] From: {self.sender.name} | Subject: {self.subject}"
-        
-class User:
-# In User class, we have 1 param
-# accesed by Email class
-    def __init__(self, name):
-        
-        # user would have a name and inbox
-        self.name = name
-        # inbox will be able to send and recieve mails
-        self.inbox = Inbox()
 
-    # instance method to send email as User    
-    def send_email(self, receiver, subject, body):
-        email = Email(sender=self, receiver=receiver, subject=subject, body=body)
-        receiver.inbox.receive_email(email)
-
-
+# Inbox
 class Inbox:
 # In Inbox class
     def __init__(self):
+# Holds the list of emails
         self.emails = []
 
     # instance method to recieve email in inbox
     def receive_email(self, email):
         self.emails.append(email)
 
+# Represents a collection of emails
     # instance method to store recieved emails in inbox
     def list_emails(self):
         # validation - for empty inbox
@@ -79,3 +69,21 @@ class Inbox:
             return
         actual_index = index - 1
         
+
+# User - Represents a person
+class User:
+# In User class, we have 1 param
+# accesed by Email class - User has a Inbox
+    def __init__(self, name):
+        
+# Has a name and an inbox
+        self.name = name
+
+        # inbox will be able to send and recieve mails
+        # obj relation - composition - Has a
+        self.inbox = Inbox()
+
+    # instance method to send email as User    
+    def send_email(self, receiver, subject, body):
+        email = Email(sender=self, receiver=receiver, subject=subject, body=body)
+        receiver.inbox.receive_email(email)
