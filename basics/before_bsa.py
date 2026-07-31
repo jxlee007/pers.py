41ba1f1be03f4724d00151a04d8c89d6b260f949
jxlee <jimmyfalco007@gmail.com>
2026-08-01 00:42:23 +0530

    OOPS-3007-3107

--- basics/before_bsa.py
+++ basics/before_bsa.py
@@ -6,16 +6,41 @@ def main():
 
 # C2.1 - The Evens Purger
 def remove_even():
-    num_list = [i + 1 for i in range(10)]
+    num_list = [i + 1 for i in range(50)]
+    print(num_list.index(1))
+    print(num_list)
+    idx = 0
+
+    while idx < len(num_list):
+        # Check the value at the current index position
+        if num_list[idx] % 2 == 0:
+            del num_list[idx]
+            # Do NOT increase idx here! The next item slid into this spot.
+        else:
+            idx += 1  # Only move forward if the item was odd
+
+    
+    numlist = [ str(i) for i in num_list]
+
+    idx = 1
+    print(numlist[idx])
+    while idx < len(numlist):
+        numlist[0] += numlist[idx]
+        idx += 1 
+
+    m = numlist[0]
+
+    print(m)
+
+
 
     # Three Loop Questions:
     #1. What do I want to repeat?
-    #  -> message
-    #2. What do I want to change each time?
     #  -> 
+    #2. What do I want to change each time?
+    #  -> index 
     #3. How long should we repeat?
-    #  ->  
-    print(num_list)
+    #  -> till 
 
 
 
--- oops/brillant.py
+++ oops/brillant.py
@@ -1,14 +1,51 @@
 #OOPS
-# 1. Create Agent class -> 2707
+# -> 2707
+# 1. Create Agent class     
 # 2. update attributes
-# 3.  -> 2807
-# 4.
+# -> 2807
+# 3. methods - pairing attrs 
+# 4. updating using methods
+# -> 3007
+# 5. retuning vals - print keeps fnc running
+# 6. lvl review
 
-def main():
-    # print(Agent)
-    pass
+# Designing classes
+# -> 3107   
+# 7. Planning classes - psuedocode
+# 8. Building states - code
+# -> note
+"""
+1. Imports go at the very top
+2. Classes are defined at the module level
+3. Main function ONLY handles execution logic
+"""
+# 9. reporting states
+
+
+# -------------------------------------------------
+# modules
+
+
+class word_counter():
+# Set up an empty dictionary of word count pairs.  - init class
+# Given a word from the dataset , update its count  - plan method
+# Return the number of times a word appears in the dataset  - plan output
+
+    def __init__(self):
+        self.count = {}
+
+# methods that change an obj state are known as mutators
+    def update_count(self,word):
+    # update-count mutator update and build up count dictionary
+        if word not in self.count :
+            self.count[word] = 1
+        else:
+            self.count[word] +=1
+
+
 
 class Agent():
+    # print(Agent)
     # dunder methods
     def __init__(self, name: str, style: str) -> None:
         self.name=name
@@ -16,30 +53,109 @@ class Agent():
         self.premium=False
         self.query_count = 0
 
-User_queries =[ # ...
-"What is the capital of Italy",
-"How do I tie a shoe",
-"Are frogs reptiles",
-"Where is the nearest car wash",
-"Should I drive or walk to it"
-]
+    def introduce(self):
+        if self.style == "helpful":
+            print("Hi, I'm" , self.name )
+            print("How can I help you today?")
+        elif self.style == "direct":
+            print("I am", self.name )
+            print("What do you want?")
+        else:
+            print("He110, I'm", self.name )  
 
-aria = Agent("Aria", "helpful")
-rex = Agent("Rex", "direct")
-zig = Agent("Zig","cautious")
-cora = Agent("Cora", "friendly")
-gert = Agent("Gert", "helpful")
+    def report_status(self): # for showing current attributes of agent
+        print (f""" 
+    Status report for {self.name}
+    Style: {self.style}
+    Queries handled: {self.query_count}
+            """)  
 
-zig.premium = True
+    def change_style(self, change: str) -> None:
+        if change in ['direct', 'helpful', 'friendly', 'cautious']:
+            self.style = change
+            print(f"{self.name} is now {self.style}")
+        else:
+            print(f'{change} is not a valid style')
 
-for agent in [aria,rex,zig,cora,gert]:
-    print(f"{agent.name} is a {agent.style} agent{'. Needs subscription' if agent.premium == True else "."} Queries handled: {agent.query_count} ")
 
-for user_query in User_queries:
-    print(f"User:{user_query}")
-    print("Zig is thinking")
-    zig.query_count =+ 1
+# -------------------------------
+# logic
 
+
+def main():
+    # agent()
+    word_count()
+
+
+def agent():
+    User_queries =[ # ...
+    "What is the capital of Italy",
+    "How do I tie a shoe",
+    "Are frogs reptiles",
+    "Where is the nearest car wash",
+    "Should I drive or walk to it"
+    ]
+
+    aria = Agent("Aria", "helpful")
+    aria.introduce()
+
+    rex = Agent("Rex", "direct")
+    rex.introduce()
+
+    zig = Agent("Zig","cautious")
+    zig.premium = True
+
+    cora = Agent("Cora", "friendly")
+    cora.introduce()
+
+    gert = Agent("Gert", "helpful")
+    gert.name = 'Gertie'
+    # gert.style = 'Charmy'
+    gert.report_status()
+    gert.change_style("Smart")
+
+    # for agent in [aria,rex,zig,cora,gert]:
+    #     print(f"{agent.name} is a {agent.style} agent
+    #             {'. Needs subscription' if agent.premium == True else "."} 
+    #             Queries handled: {agent.query_count} ")
+
+    for user_query in User_queries:
+        print(f"User:{user_query}")
+        print(f"{gert.name} is thinking")
+        gert.query_count += 1
+
+    gert.report_status()
+
+
+def word_count():
+
+    cow_data = ("hey diddle diddle "
+        "the cat and the fiddle "
+        "the cow jumped over the moon "
+        "the little dog laughed "
+        "to see such sport "
+        "and the dish ran away with the spoon")
+
+    word_list_2 = cow_data.split()
+    print(word_list_2)
+    cow_counter = word_counter()
+    for word in word_list_2:
+        cow_counter.update_count(word)
+    print(cow_counter.count)
+
+    # word_list=['the','cow','jumped','over','the',"moon"]
+    # list_counter = word_counter()
+    
+    # for word in word_list:
+    #     list_counter.update_count(word)
+    # print(list_counter.count)
+        
+    # counter = word_counter()
+    # print(counter.count)
+    # counter.update_count("the")
+    # print(counter.count)
+    # counter.update_count("the")
+    # print(counter.count)
 
 
 
